ca83989f2da6ce5970cf78d42cd50030afbd051d
Matt Perry <mperry120@gmail.com>
2024-04-12 12:08:43 -0700

    GUIs

--- PyGUIS/MessageBoxes.py
+++ PyGUIS/MessageBoxes.py
@@ -21,9 +21,48 @@ class Window(QWidget):
 
         self.show()
 
+    # #First Workable
+    # def messageBox(self):
+    #     mBox = QMessageBox(self)
+    #     yesButton = mBox.addButton(QMessageBox.StandardButton.Yes)
+    #     mBox.exec()
+    #     if mBox.clickedButton() == yesButton:
+    #         print("TEST")
+
+    #Second Workable
     def messageBox(self):
-        mBox = QMessageBox()
-        mBox.setInformativeText("test")
+        mBox = QMessageBox.question(self, 
+                                    "Warning", 
+                                    "Are you sure you want to exit?", 
+                                    buttons=QMessageBox.StandardButton.Yes | 
+                                    QMessageBox.StandardButton.Save | 
+                                    QMessageBox.StandardButton.Abort | 
+                                    QMessageBox.StandardButton.Apply, 
+                                    defaultButton= QMessageBox.StandardButton.Abort
+                                    )
+        if mBox == QMessageBox.StandardButton.Yes:
+            print('Clicked Yes')
+            extBox = QMessageBox.information(self, "Notice", "Exiting application")
+            if extBox == QMessageBox.StandardButton.Ok:
+                sys.exit()
+            else:
+                sys.exit()
+        elif mBox == QMessageBox.StandardButton.Save:
+            txtBox = QMessageBox.information(self, "Notice", "You clicked Save.. What are you trying to save?")
+            print('Clicked Save, but there\'s nothing to save, lol')
+        elif mBox == QMessageBox.StandardButton.Abort:
+            print('Clicked Abort')
+        elif mBox == QMessageBox.StandardButton.Apply:
+            txtBox = QMessageBox.information(self, "Notice", "You clicked Apply.. There's nothing to apply...")
+            print("Clicked Apply, but there's nothing to apply, lol")
+        #bug... doc says it whould return 'escape button' if none of the set standard buttons are clicked but...
+        else:
+            print('Pressed ESC')        
+        
+        
+
+
+        
 
 
 App = QApplication(sys.argv)
