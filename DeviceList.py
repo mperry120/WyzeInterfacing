47246e0944ec11a6a4bcb4b5abce8f97d02981d1
Matt Perry <mperry120@gmail.com>
2024-04-16 00:14:28 -0700

    minor changes

--- DeviceList.py
+++ DeviceList.py
@@ -1,11 +1,11 @@
 import os
 import sys
-
 import datetime
 import time
 import calendar
 import pprint
 import func
+import SaveData
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import Qt
 from wyze_sdk.errors import WyzeApiError
@@ -28,19 +28,19 @@ try:
     class Window(QWidget):
         def __init__(self):
             super().__init__()
-            self.setWindowTitle("Horizontal Box Layout")
+            self.setWindowTitle("Wyze Data Portal")
             self.setGeometry(350,150,400,400)
             self.UI()
 
         def UI(self):
             mainLayout = QVBoxLayout()
             TopLayout = QVBoxLayout()
-            BottomLayout = QVBoxLayout()
+            BottomLayout = QHBoxLayout()
             mainLayout.addLayout(TopLayout)
             mainLayout.addLayout(BottomLayout)
 
-            email = QLineEdit()
-            email.setPlaceholderText("Email")
+            self.email = QLineEdit()
+            self.email.setPlaceholderText("Email")
             password = QLineEdit()
             password.setPlaceholderText("Password")
             key_id = QLineEdit()
@@ -51,27 +51,47 @@ try:
             TopLayout.addStretch()
             TopLayout.addWidget(QLabel("Enter your Wyze credentials:"), alignment=Qt.AlignmentFlag.AlignCenter)
 
-            TopLayout.addWidget(email, alignment=Qt.AlignmentFlag.AlignCenter)
+            TopLayout.addWidget(self.email, alignment=Qt.AlignmentFlag.AlignCenter)
             TopLayout.addWidget(password, alignment=Qt.AlignmentFlag.AlignCenter)
             TopLayout.addWidget(key_id, alignment=Qt.AlignmentFlag.AlignCenter)
             TopLayout.addWidget(api_key, alignment=Qt.AlignmentFlag.AlignCenter)
             TopLayout.addStretch()
             
-            hBox = QHBoxLayout()
-
-            BottomLayout.addLayout(hBox)
-
-            hBox.addStretch()
-            hBox.addWidget(QCheckBox("Remember Me"))
-            hBox.addWidget(button)
-            hBox.addStretch()
+            self.rememberMe = QCheckBox("Remember Me")
+            BottomLayout.addWidget(self.rememberMe, alignment=Qt.AlignmentFlag.AlignRight)
+            BottomLayout.addWidget(button, alignment=Qt.AlignmentFlag.AlignLeft)
             
-            BottomLayout.addStretch()
 
+            button.clicked.connect(self.pressEnter)
 
+            self.rememberMe.isChecked
+
+            # hBox = QHBoxLayout()
+
+            # BottomLayout.addLayout(hBox)
+
+            # hBox.addStretch()
+            # hBox.addWidget(QCheckBox("Remember Me"))
+            # hBox.addWidget(button)
+            # hBox.addStretch()
+            
+            # BottomLayout.addStretch()
+
+            self.email.setText(SaveData.email)
             self.setLayout(mainLayout)
             self.show()
 
+        def pressEnter(self):
+            if self.rememberMe.isChecked():
+                SaveData.saveData(self.email.text())
+            
+            # password = password.text()
+            # key_id = key_id.text()
+            # api_key = api_key.text()
+            # rememberMe = rememberMe.isChecked()
+            print(self.email.text())
+            # SaveData.saveData(email, password, key_id, api_key, rememberMe)
+            # self.close()
 
     def main():
         App=QApplication(sys.argv)
---
+++ SaveData.py
@@ -0,0 +1,13 @@
+email = ""
+
+def saveData(eMail):
+    #global name
+    email = eMail
+    # with open("data.txt", "w") as file:
+    #     file.write(email + "\n")
+    #     file.write(password + "\n")
+    #     file.write(key_id + "\n")
+    #     file.write(api_key + "\n")
+    #     file.write(str(rememberMe) + "\n")
+    #     file.close()
+    #     print("Data saved")
